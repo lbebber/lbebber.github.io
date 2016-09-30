9cf4a93cc8f23e077106baef8b813ef275684e67
Lucas Bebber <lucasbbebber@gmail.com>
2016-09-29 21:21:50 -0300

    save point

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,11 @@
     blackish : '#1B2947',
     green : '#54fad4',
   }
+  var dpi=window.devicePixelRatio;
   function createCanvas(width,height){
     var canvas=document.createElement('canvas');
-    setAttribute('width',width,canvas);
-    setAttribute('height',height,canvas);
+    setAttribute('width',width*dpi,canvas);
+    setAttribute('height',height*dpi,canvas);
     return canvas;
   }
   function drawStar(x,y,size,scale,ctx){
@@ -23,6 +24,10 @@
   }
   function querySelector(selector){
     return document.querySelector(selector);
+  }
+  function querySelectorAll(selector){
+    var nodes=document.querySelectorAll(selector);
+    return [].slice.call(nodes);
   }
   function getContext(canvas){
     return canvas.getContext('2d');
@@ -53,188 +58,204 @@
     }
   }
   var raf=requestAnimationFrame;
-  function sizeToBounds(bounds,element){
-    setAttribute('width',bounds.width,element);
-    setAttribute('height',bounds.height,element);
+  function sizeToBounds(bounds,dpi,element){
+    setAttribute('width',bounds.width*dpi,element);
+    setAttribute('height',bounds.height*dpi,element);
   }
 
   (function(){
-    var canvas=querySelector('.Scene-mountains');
-    var bounds=getBounds(canvas);
-    sizeToBounds(bounds,canvas);
-    var middle={
-      x:bounds.width/2,
-      y:bounds.height/2
-    }
-    var textureMountains=createMountains();
-    var createdGL=createGL();
+    var canvases=querySelectorAll('.Scene-mountains');
+    forEach(canvases,function(canvas){
+      var bounds=getBounds(canvas);
+      sizeToBounds(bounds,dpi,canvas);
+      var middle={
+        x:bounds.width/2,
+        y:bounds.height/2
+      }
+      var textureMountains=createMountains();
+      var createdGL=createGL();
 
-    function createGL(){
-      var vertexShader = document.getElementById('vs').textContent;
-      var fragmentShader = document.getElementById('fs').textContent;
-      var currentProgram;
-      var timeLocation;
-      var resolutionLocation;
-      var buffer;
-      var gl;
-      var startTime=new Date().getTime();
-      var time=0;
+      function createGL(){
+        var vertexShader = document.getElementById('vs').textContent;
+        var fragmentShader = document.getElementById('fs').textContent;
+        var currentProgram;
+        var timeLocation;
+        var resolutionLocation;
+        var buffer;
+        var gl;
+        var startTime=new Date().getTime();
+        var time=0;
 
-      if(!init()) return false;
-      animate();
+        if(!init()) return false;
+        animate();
 
-      function init(){
-        try {
-          gl = canvas.getContext('experimental-webgl',{
-            premultipliedAlpha:false
-          });
-        } catch( error ) { }
-        if ( !gl ) {
-          return false;
+        function init(){
+          try {
+            gl = canvas.getContext('experimental-webgl',{
+              premultipliedAlpha:false
+            });
+          } catch( error ) { }
+          if ( !gl ) {
+            return false;
+          }
+          buffer = gl.createBuffer();
+          gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+          gl.bufferData(gl.ARRAY_BUFFER, new Float32Array( [ - 1.0, - 1.0, 1.0, - 1.0, - 1.0, 1.0, 1.0, - 1.0, 1.0, 1.0, - 1.0, 1.0 ] ), gl.STATIC_DRAW );
+          currentProgram = createProgram(vertexShader, fragmentShader);
+          if(currentProgram==null) return false;
+          timeLocation = gl.getUniformLocation(currentProgram, 'time');
+          resolutionLocation = gl.getUniformLocation(currentProgram, 'resolution');
+
+          // Create a texture.
+          var texture = gl.createTexture();
+          gl.bindTexture(gl.TEXTURE_2D, texture);
+         
+          // Set the parameters so we can render any size image.
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+          gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+          // Upload the image into the texture.
+          gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureMountains);
+
+          return true;
         }
-        buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array( [ - 1.0, - 1.0, 1.0, - 1.0, - 1.0, 1.0, 1.0, - 1.0, 1.0, 1.0, - 1.0, 1.0 ] ), gl.STATIC_DRAW );
-        currentProgram = createProgram(vertexShader, fragmentShader);
-        if(currentProgram==null) return false;
-        timeLocation = gl.getUniformLocation(currentProgram, 'time');
-        resolutionLocation = gl.getUniformLocation(currentProgram, 'resolution');
 
-        // Create a texture.
-				var texture = gl.createTexture();
-				gl.bindTexture(gl.TEXTURE_2D, texture);
-			 
-				// Set the parameters so we can render any size image.
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-				gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-				// Upload the image into the texture.
-        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, textureMountains);
+        function createProgram( vertex, fragment ) {
+          var program = gl.createProgram();
 
+          var vs = createShader( vertex, gl.VERTEX_SHADER );
+          var fs = createShader( '#ifdef GL_ES\nprecision highp float;\n#endif\n\n' + fragment, gl.FRAGMENT_SHADER );
+          if ( vs == null || fs == null ) return null;
+
+          gl.attachShader( program, vs );
+          gl.attachShader( program, fs );
+
+          gl.deleteShader( vs );
+          gl.deleteShader( fs );
+
+          gl.linkProgram( program );
+
+          if ( !gl.getProgramParameter( program, gl.LINK_STATUS ) ) {
+            console.log( "ERROR:\n" +
+            "VALIDATE_STATUS: " + gl.getProgramParameter( program, gl.VALIDATE_STATUS ) + "\n" +
+            "ERROR: " + gl.getError() + "\n\n" +
+            "- Vertex Shader -\n" + vertex + "\n\n" +
+            "- Fragment Shader -\n" + fragment );
+            return null;
+          }
+          return program;
+        }
+
+        function createShader(src, type) {
+          var shader = gl.createShader( type );
+
+          gl.shaderSource( shader, src );
+          gl.compileShader( shader );
+
+          if ( !gl.getShaderParameter( shader, gl.COMPILE_STATUS ) ) {
+            console.log( ( type == gl.VERTEX_SHADER ? "VERTEX" : "FRAGMENT" ) + " SHADER:\n" + gl.getShaderInfoLog( shader ) );
+            return null;
+          }
+          return shader;
+        }
+        
+        function animate(){
+          render();
+          raf(animate);
+        }
+
+        function render(){
+          if ( !currentProgram ) return;
+
+          time=new Date().getTime()-startTime;
+
+          gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+
+          gl.useProgram(currentProgram);
+
+          gl.uniform1f(timeLocation, time/1000);
+          gl.uniform2f(resolutionLocation, bounds.width*dpi,bounds.height*dpi);
+
+          gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
+
+          var vertexPosition;
+          gl.vertexAttribPointer( vertexPosition, 2, gl.FLOAT, false, 0, 0 );
+          gl.enableVertexAttribArray(vertexPosition);
+          gl.drawArrays(gl.TRIANGLES, 0, 6);
+          gl.disableVertexAttribArray(vertexPosition);
+        }
         return true;
       }
 
-      function createProgram( vertex, fragment ) {
-        var program = gl.createProgram();
-
-        var vs = createShader( vertex, gl.VERTEX_SHADER );
-        var fs = createShader( '#ifdef GL_ES\nprecision highp float;\n#endif\n\n' + fragment, gl.FRAGMENT_SHADER );
-        if ( vs == null || fs == null ) return null;
-
-        gl.attachShader( program, vs );
-        gl.attachShader( program, fs );
-
-        gl.deleteShader( vs );
-        gl.deleteShader( fs );
-
-        gl.linkProgram( program );
-
-        if ( !gl.getProgramParameter( program, gl.LINK_STATUS ) ) {
-          console.log( "ERROR:\n" +
-          "VALIDATE_STATUS: " + gl.getProgramParameter( program, gl.VALIDATE_STATUS ) + "\n" +
-          "ERROR: " + gl.getError() + "\n\n" +
-          "- Vertex Shader -\n" + vertex + "\n\n" +
-          "- Fragment Shader -\n" + fragment );
-          return null;
+      function createMountains(){
+        var textureCanvas=createCanvas(bounds.width,bounds.height);
+        sizeToBounds(bounds,dpi,textureCanvas);
+        var ctx=getContext(textureCanvas);
+        var factor=18;
+        var cols=Math.round(bounds.width/factor);
+        if(bounds.width>1200){
+          cols=55;
         }
-        return program;
-      }
-
-      function createShader(src, type) {
-        var shader = gl.createShader( type );
-
-        gl.shaderSource( shader, src );
-        gl.compileShader( shader );
-
-        if ( !gl.getShaderParameter( shader, gl.COMPILE_STATUS ) ) {
-          console.log( ( type == gl.VERTEX_SHADER ? "VERTEX" : "FRAGMENT" ) + " SHADER:\n" + gl.getShaderInfoLog( shader ) );
-          return null;
+        var gridSize=bounds.width/cols;
+        var mountains={
+          left:[
+            '......kkkkjjjkkjjkjkkjjkkkkjjjjj',
+            '....kkkjjj..kkkkjkkjjjj.....kkkkkjjjjjj',
+            '..kkjj....kkkkjjjjkkkkjkjjjj',
+          ],
+          right:[
+            '......kkkkjjjkkjjkjkkjjkkkkkjjjjjj',
+            '....kkkjjj..kkkkjkkjjjj.....kkkkkjjjjjj',
+            '..kkjj....kkkkjjjjkkkkjkjjjj',
+          ]
         }
-        return shader;
+        function drawLayer(layer,directionX,directionY){
+          ctx.beginPath();
+          var pos=0;
+          var started=false;
+          repeat(layer.length,function(i){
+            var dir=layer.charAt(i);
+            pos+=dir=='.'?0:(dir=='j'?-1:1);
+            if(pos!=0 && !started){
+              started=true;
+              ctx.moveTo((middle.x+((i-1)*gridSize*directionX))*dpi,middle.y*dpi);
+            }
+            ctx.lineTo(
+              (middle.x+(i*gridSize)*directionX)*dpi,
+              (middle.y+(pos*gridSize)*directionY)*dpi
+            )
+          });
+          ctx.closePath();
+          ctx.fill();
+          // ctx.stroke();
+        }
+        function drawLayers(i,color1,color2){
+          setFillStyle(color1,ctx);
+          ctx.strokeStyle=color1;
+          ctx.lineWidth=4;
+          ctx.lineJoin='round'
+          drawLayer(mountains.left[i],-1,-1);
+          drawLayer(mountains.right[i],1,-1);
+          setFillStyle(color2,ctx);
+          ctx.strokeStyle=color2;
+          drawLayer(mountains.left[i],-1,1);
+          drawLayer(mountains.right[i],1,1);
+          ctx.strokeStyle='transparent';
+        }
+        drawLayers(2,colors.pink,colors.green);
+        drawLayers(1,colors.purple,colors.pink);
+        drawLayers(0,colors.blackish,colors.purple);
+        return textureCanvas;
       }
-      
-      function animate(){
-        render();
-        raf(animate);
-      }
-
-      function render(){
-        if ( !currentProgram ) return;
-
-        time=new Date().getTime()-startTime;
-
-        gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-
-        gl.useProgram(currentProgram);
-
-        gl.uniform1f(timeLocation, time/1000);
-        gl.uniform2f(resolutionLocation, bounds.width,bounds.height);
-
-        gl.bindBuffer( gl.ARRAY_BUFFER, buffer );
-
-        var vertexPosition;
-        gl.vertexAttribPointer( vertexPosition, 2, gl.FLOAT, false, 0, 0 );
-        gl.enableVertexAttribArray(vertexPosition);
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
-        gl.disableVertexAttribArray(vertexPosition);
-      }
-      return true;
-    }
-
-    function createMountains(){
-      var textureCanvas=createCanvas(bounds.width,bounds.height);
-      sizeToBounds(bounds,textureCanvas);
-      var ctx=getContext(textureCanvas);
-      var cols=60;
-      var gridSize=bounds.width/cols;
-      var mountains={
-        left:[
-          '......kkkkjjjkkjjkjkkjjkkkkkjjjjjj',
-          '....kkkjjj..kkkkjkkjjjj.....kkkkkjjjjjj',
-          '..kkjj....kkkkjjjjkkkkjkjjjj',
-        ],
-        right:[
-          '......kkkkjjjkkjjkjkkjjkkkkkjjjjjj',
-          '....kkkjjj..kkkkjkkjjjj.....kkkkkjjjjjj',
-          '..kkjj....kkkkjjjjkkkkjkjjjj',
-        ]
-      }
-      function drawLayer(layer,directionX,directionY){
-        ctx.beginPath();
-        ctx.moveTo(middle.x,middle.y);
-        var pos=0;
-        repeat(layer.length,function(i){
-          var dir=layer.charAt(i);
-          pos+=dir=='.'?0:(dir=='j'?-1:1);
-          ctx.lineTo(
-            middle.x+(i*gridSize)*directionX,
-            middle.y+(pos*gridSize)*directionY
-          )
-        });
-        ctx.closePath();
-        ctx.fill();
-      }
-      function drawLayers(i,color1,color2){
-        setFillStyle(color1,ctx);
-        drawLayer(mountains.left[i],-1,-1);
-        drawLayer(mountains.right[i],1,-1);
-        setFillStyle(color2,ctx);
-        drawLayer(mountains.left[i],-1,1);
-        drawLayer(mountains.right[i],1,1);
-      }
-      drawLayers(2,colors.pink,colors.green);
-      drawLayers(1,colors.purple,colors.pink);
-      drawLayers(0,colors.blackish,colors.purple);
-      return textureCanvas;
-    }
+    });
   }());
 
   ;(function(){
     var canvas=querySelector('.Scene-stars');    
     var ctx=getContext(canvas);
     var bounds=getBounds(canvas);
-    sizeToBounds(bounds,canvas);
+    sizeToBounds(bounds,dpi,canvas);
     var stars=[];
     function createStar(){
       return {
@@ -248,7 +269,7 @@
     }
 
     ;(function draw(){
-      ctx.clearRect(0,0,bounds.width,bounds.height);
+      ctx.clearRect(0,0,bounds.width*dpi,bounds.height*dpi);
       setFillStyle(colors.whiteish,ctx);
       var newStars=[];
       forEach(stars,function(star){
@@ -260,18 +281,24 @@
           newStars.push(star);
         }
         drawStar(
-          star.x,
-          star.y,
+          star.x*dpi,
+          star.y*dpi,
           1,
-          smooth(star.s)*star.maxSize,
+          smooth(star.s)*star.maxSize*dpi,
           ctx
         );
       });
-      if(random(1)<0.5) newStars.push(createStar());
+      if(random(1)<0.3) newStars.push(createStar());
       stars=newStars;
       raf(draw);
     }());
 
   }());
-}());
 
+  (function(){
+    forEach(querySelectorAll('.js-HasVH'),function(el){
+      var bounds=getBounds(el);
+      el.style.height=bounds.height+'px';
+    });
+  }());
+}());
